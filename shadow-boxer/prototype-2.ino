7bd71fa0e14a8d621ed36d2374773b083ced2372
Kyle Corry <kylecorry31@gmail.com>
2021-02-21 07:56:30 -0500

    Add single accelerometer version

---
+++ shadow-boxer/prototype-2.ino
@@ -0,0 +1,31 @@
+#include <Adafruit_MPU6050.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial bluetooth(10, 11); // RX | TX
+
+Adafruit_MPU6050 mpu{};
+
+void setup() {
+  Serial.begin(9600);
+  bluetooth.begin(9600);
+
+  while (!mpu.begin()){
+    delay(10);
+  }
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
+  mpu.setFilterBandwidth(MPU6050_BAND_44_HZ);
+}
+
+void loop() {
+
+  sensors_event_t mpuA, mpuG, mpuTemp;
+
+  mpu.getEvent(&mpuA, &mpuG, &mpuTemp);
+
+  bluetooth.print(mpuA.acceleration.x);
+  bluetooth.print(',');
+  bluetooth.print(mpuA.acceleration.y);
+  bluetooth.print(',');
+  bluetooth.println(mpuA.acceleration.z);
+}
