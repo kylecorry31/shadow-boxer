f721016b5eca8c30d9404ae264e48890e0b78c19
Kyle Corry <kylecorry31@gmail.com>
2021-03-05 10:16:53 -0500

    Update rx and tx ports

--- shadow-boxer/prototype-2/prototype-2.ino
+++ shadow-boxer/prototype-2/prototype-2.ino
@@ -1,17 +1,17 @@
 #include <Adafruit_MPU6050.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial bluetooth(10, 11); // RX | TX
+SoftwareSerial bluetooth(11, 10); // RX | TX
 
 Adafruit_MPU6050 mpu{};
 
 void setup() {
-  Serial.begin(9600);
   bluetooth.begin(9600);
 
   while (!mpu.begin()){
     delay(10);
   }
+
 
   mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
   mpu.setFilterBandwidth(MPU6050_BAND_44_HZ);
@@ -22,7 +22,6 @@ void loop() {
   sensors_event_t mpuA, mpuG, mpuTemp;
 
   mpu.getEvent(&mpuA, &mpuG, &mpuTemp);
-
   bluetooth.print(mpuA.acceleration.x);
   bluetooth.print(',');
   bluetooth.print(mpuA.acceleration.y);
